2aa33c7091dc9007554d571bc79a250507e87507
taalai-z <taalai.z01@gmail.com>
2022-07-14 17:07:52 +0600

    Home work done

---
+++ Lesson 2 (variables)/main.dart
@@ -0,0 +1,29 @@
+void main(List<String> arguments) {
+// First task
+  int a = 3;
+  double b = 4.20;
+  double sum = a.toDouble() + b;
+
+  print(sum);
+
+// Second task
+  String text = "Name";
+  text = "Flutter";
+
+  print('I want to learn $text');
+
+// Third task
+  double myNumber = 2.225;
+  String myText = "";
+  myText = myNumber.toString();
+
+  print(myText);
+
+  String name = "Taalai";
+  print('Your name is $name');
+
+  String changes = "Hello ";
+  String newtest = "asxxx";
+
+  int numberrrrs = 43434343434343434;
+}
