4e63dd8deef904d9c538a6b83a05c38e6d00e29d
taalai-z <taalai.z01@gmail.com>
2022-07-22 12:05:07 +0600

    can do checking var's

--- Lesson 6/main.dart
+++ Lesson 6/main.dart
@@ -1,16 +1,20 @@
 import 'dart:io';
 
 void main() {
+  double result = 0;
+
   print("Введите первое число");
   double firstNumber = double.parse(stdin.readLineSync()!);
 
-  print("Введите действие: \n+ \n- \n* \n/ ");
+  print("Введите действие: \n+ \n- \n* \n/ \n^2");
   String operation = stdin.readLineSync()!;
 
+  if (operation == '^2') {
+    print(firstNumber * firstNumber);
+    exit(200);
+  }
   print("Введите второе число");
   double SecondNumber = double.parse(stdin.readLineSync()!);
-
-  double result = 0;
 
   switch (operation) {
     case '+':
